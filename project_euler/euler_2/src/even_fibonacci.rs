082e309184964be9804ec87abb81a1a61cb1ab7b
Nick Dolan <nicholas.dolan1@gmail.com>
2019-01-16 16:03:46 -0600

    refactor(fibonacci): Replaced Even Fibonacci with Fibonacci + Filter

--- project_euler/euler_2/src/even_fibonacci.rs
+++ project_euler/euler_2/src/even_fibonacci.rs
@@ -5,46 +5,36 @@ pub fn go(n: u32) -> u64 {
 
     let mut sum: u64 = 0;
 
-    for i in evenFibonacci().take(n as usize){
+    for i in fibonacci()
+        .filter(|i| i % 2 == 0)
+        .take(n as usize){
         sum += i as u64;
     }
 
     return sum;
 }
 
-// Nicholas Dolan
-// 2018-11-16
-// Modification #1
-struct EvenFibonacci{
+struct Fibonacci {
     curr: u32,
     next: u32
 }
 
-impl Iterator for EvenFibonacci {
+impl Iterator for Fibonacci {
     type Item = u32;
 
-    fn next(&mut self) -> Option<u32> {
-
+    fn next( &mut self ) -> Option<u32> {
         if self.next >= 4000000 {
             return None
         }
 
         let t = self.curr + self.next;
-
         self.curr = self.next;
         self.next = t;
-
-        while self.curr % 2 != 0 {
-            let t = self.curr + self.next;
-
-            self.curr = self.next;
-            self.next = t;
-        }
 
         Some(self.curr)
     }
 }
 
-fn evenFibonacci() -> EvenFibonacci {
-    EvenFibonacci {curr: 1, next: 2}
+fn fibonacci() -> Fibonacci {
+    Fibonacci { curr: 1, next: 1}
 }
